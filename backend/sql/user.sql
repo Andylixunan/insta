860cdbf2cd8ba31459d8c909af2fc941c66b2624
LI Xunan <xl2216@nyu.edu>
2022-02-26 01:02:14 +0800

    small updates for sql

--- backend/sql/user.sql
+++ backend/sql/user.sql
@@ -4,6 +4,8 @@ CREATE TABLE users
     username   VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     nickname   VARCHAR(255) NOT NULL DEFAULT 'no nickname',
+    self_description VARCHAR(255) NOT NULL DEFAULT 'no self_description',
+    avatar VARCHAR(255) COMMENT 'URL of the avatar',
     created_at DATETIME(3),
     updated_at DATETIME(3),
     deleted_at DATETIME(3) NULL
