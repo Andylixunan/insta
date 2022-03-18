9c8e66050a5c438d031038510f0aa86f234c9db4
LI Xunan <xl2216@nyu.edu>
2022-03-19 02:04:47 +0800

    initialize auth service

--- internal/auth/server.go
+++ internal/auth/server.go
@@ -1,1 +1,17 @@
 package auth
+
+import (
+	pb "github.com/Andylixunan/insta/api/proto/auth"
+	"github.com/Andylixunan/insta/pkg/log"
+)
+
+type server struct {
+	pb.UnimplementedAuthServiceServer
+	logger *log.Logger
+}
+
+func NewServer(logger *log.Logger) pb.AuthServiceServer {
+	return &server{
+		logger: logger,
+	}
+}
