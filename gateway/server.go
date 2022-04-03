95e334a18dc91b923507449cc5578ff4cc327ecc
LI Xunan <xl2216@nyu.edu>
2022-04-04 03:22:56 +0800

    add test for middleware

--- gateway/server.go
+++ gateway/server.go
@@ -24,5 +24,10 @@ func NewServer(config *config.Config, logger *log.Logger) *Server {
 		accountAPIGroup.POST("/register", register)
 		accountAPIGroup.POST("/login", login)
 	}
+	// server.POST("/test", middleware.AuthMiddleware(config, logger), func(ctx *gin.Context) {
+	// 	id, ok := ctx.Get("UserID")
+	// 	logger.Infof("%v, %v", ok, id)
+	// 	ctx.JSON(http.StatusOK, gin.H{"msg": id.(uint32)})
+	// })
 	return server
 }
